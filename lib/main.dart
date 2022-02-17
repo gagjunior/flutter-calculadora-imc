c04d1a70292fc0a8d83f5b9993d35a57f098b079
Gilberto Junior <gagjunior@hotmail.com>
2022-02-16 22:46:30 -0300

    Adicionando TextFields

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Home(),
   ));
@@ -19,20 +19,57 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'Calculadora de IMC',
         ),
         centerTitle: true,
         backgroundColor: Colors.green,
         actions: [
           IconButton(
-            onPressed: (){},
+            onPressed: () {},
             icon: Icon(Icons.refresh),
           ),
         ],
       ),
       backgroundColor: Colors.white,
-      body: ,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const [
+          Icon(
+            Icons.person_outline,
+            size: 120,
+            color: Colors.green,
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.green,
+              fontSize: 25.0,
+            ),
+            decoration: InputDecoration(
+              labelText: 'Peso (Kg)',
+              labelStyle: TextStyle(
+                color: Colors.green,
+              ),
+            ),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.green,
+              fontSize: 25.0,
+            ),
+            decoration: InputDecoration(
+              labelText: 'Altura (cm)',
+              labelStyle: TextStyle(
+                color: Colors.green,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
