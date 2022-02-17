b8a3d90bdf21f765455c2afb68584e7f01418724
Gilberto Junior <gagjunior@hotmail.com>
2022-02-16 23:58:06 -0300

    Iplementando calculo e a exibição do IMC

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Home(),
   ));
@@ -15,6 +15,32 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  String _infoText = 'Informe seus dados';
+
+  void _resetFields() {
+    setState(() {
+    weightController.text = '';
+    heightController.text = '';
+    _infoText = 'Informe seus dados';      
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      if (imc < 18.6) {
+        _infoText = 'Abaixo do peso (${imc.toStringAsPrecision(4)})';
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = 'Peso ideal (${imc.toStringAsPrecision(4)})';
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,14 +52,14 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.green,
         actions: [
           IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.refresh),
+            onPressed: _resetFields,
+            icon: const Icon(Icons.refresh),
           ),
         ],
       ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: 12),
+        padding: const EdgeInsets.symmetric(horizontal: 12),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
@@ -42,7 +68,8 @@ class _HomeState extends State<Home> {
               size: 120,
               color: Colors.green,
             ),
-            const TextField(
+            TextField(
+              controller: weightController,
               keyboardType: TextInputType.number,
               textAlign: TextAlign.center,
               style: TextStyle(
@@ -56,7 +83,8 @@ class _HomeState extends State<Home> {
                 ),
               ),
             ),
-            const TextField(
+            TextField(
+              controller: heightController,
               keyboardType: TextInputType.number,
               textAlign: TextAlign.center,
               style: TextStyle(
@@ -71,13 +99,13 @@ class _HomeState extends State<Home> {
               ),
             ),
             Container(
-              margin: EdgeInsets.symmetric(vertical: 10),
+              margin: const EdgeInsets.symmetric(vertical: 10),
               height: 50,
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   primary: Colors.green,
                 ),
-                onPressed: () {},
+                onPressed: _calculate,
                 child: const Text(
                   'Calcular',
                   style: TextStyle(
@@ -88,7 +116,7 @@ class _HomeState extends State<Home> {
               ),
             ),
             Text(
-              'Info',
+              _infoText,
               textAlign: TextAlign.center,
               style: TextStyle(
                 color: Colors.green,
